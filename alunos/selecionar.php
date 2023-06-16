c5b6b43050d754dfa49374a4afcb0d18b1861e37
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2023-06-15 21:18:08 -0300

    .

--- alunos/selecionar.php
+++ alunos/selecionar.php
@@ -11,13 +11,33 @@ include "../includes/conexao.php";
 <p>
    <a href="novo.php"> Adicionar Aluno </a>
 </p>
-<h2>Lsitagem de alunos</h2>
+<h2>Listagem de alunos</h2>
 
 <table border="1">
    <tr>
       <td>Código</td>
       <td>Nome</td>
    </tr>
+   <?php
+   $sql = "select * from tb_alunos";
+   $todos_os_alunos = mysqli_query($conexao, $sql);
+   while($um_aluno = mysqli_fetch_assoc($todos_os_alunos)):
+      ?>
+      <tr>
+
+         <td><?php echo $um_aluno['id']?></td>
+         <td><?php echo $um_aluno['nome']?></td>
+         <td><?php echo $um_aluno['sobrenome']?></td>
+         <td><?php echo $um_aluno['telefone']?></td>
+         <td><?php echo $um_aluno['data_nascimento']?></td>
+         <td><?php echo $um_aluno['email']?></td>
+         <td><?php echo $um_aluno['senha']?></td>
+         <td><?php echo $um_aluno['foto']?></td>
+      
+      </tr>
+      <?php
+   endwhile;
+   ?>
 </table>
 <?php
 include "../includes/rodape.php";
