5e465deff3f24c4147e078459b2337cac05c183d
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2023-06-20 20:15:14 -0300

    .

---
+++ login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+    <title> Login </title>
+
+</head>
+<body>
+    
+<form method="post" action="validar.php">
+    <input  name="usuario" type="email" placeholder="Email" required>
+    <input  name="senha" type="password" placeholder="senha" required maxlength="20">
+    <button type="submit"> Acessar Sistema</button>
+</form>
+<?php
+echo "Olá";
+?>
+</body>
+</html>
