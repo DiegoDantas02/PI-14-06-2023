d583db4c2c9eee06d1511e8268e76f90c7569923
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2023-06-21 21:38:26 -0300

    .

--- alunos/editar.php
+++ alunos/editar.php
@@ -4,14 +4,20 @@ include "../includes/conexao.php";
 include "../includes/menu.php";
 
 $id = $_GET["id"];
-?>
 
+$nome = "";
+$sql = "select * from tb_alunos where id = $id";
+$todos_os_alunos = mysqli_query($conexao, $sql);
+while($um_aluno = mysqli_fetch_assoc($todos_os_alunos)):
+$nome = $um_aluno["nome"];
+endwhile;
+?>
 <h1>Editar aluno <?php echo $id; ?></h1>
 
 <form method="post" action="atualizar.php">
     <!-- mostrar no input a variavel -->
     Nome: <input name="nome" value="<?php echo $nome;?>"> <br>
-
+<br>
     <button type="submit">atualizar</button>
 </form>
 
