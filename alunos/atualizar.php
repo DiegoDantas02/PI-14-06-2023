1127a14de66747e9110ccf759acb6d445dd419bf
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2023-06-21 22:25:02 -0300

    .

--- alunos/atualizar.php
+++ alunos/atualizar.php
@@ -1,12 +1,13 @@
 <?php
 include "../includes/conexao.php";
+
 $id = $_GET["id"];
 
 $nome = $_POST["nome"];
 $sql = "update tb_alunos set nome = '$nome' where id =$id";
-
-
+mysqli_query($conexao, $sql);
 mysqli_close($conexao);
 
-include "../includes/rodape.php";
+header("location: selecionar.php");
+
 ?>
